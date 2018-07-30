35bbe80c7f04feefae39d57bdcb5c09efa6c4dc5
weixiaoyi <xiaoyi.wei@bcsystech.com>
2018-07-30 21:06:26 +0800

    我的账户

--- src/routes/User/account/MyAccount.js
+++ src/routes/User/account/MyAccount.js
@@ -11,6 +11,16 @@ import styles from './MyAccount.less'
   dispatch
 }))
 export default class View extends Component {
+  renderStatus = (right) => {
+    return right?(
+      <div className={styles.right} >
+        √
+      </div >
+    ):(
+      <div>ahhah</div>
+    )
+  }
+
   render() {
     return (
       <div className={styles.myaccount} >
@@ -23,7 +33,38 @@ export default class View extends Component {
             <div >最后登录时间 :<span >2018-04-25</span ></div >
             <div >Ip :<span >121.29.15.199</span ></div >
           </div >
-
+        </div >
+        <div className={styles.down} >
+          <div className={styles.safety} >
+            <div className={styles.title} >安全设置</div >
+            <ul >
+              <li >
+                <div className={styles.name} >登录密码</div >
+                <div className={classNames(
+                  styles.desc,
+                  styles.loginpassword
+                )} >
+                  已经设置
+                  <div className={styles.right} >
+                    √
+                  </div >
+                </div >
+                <div className={
+                  classNames(
+                    styles.button,
+                    styles.login
+                  )
+                } >
+                  修改
+                </div >
+              </li >
+              <li >
+                <div className={styles.name} >谷歌验证码</div >
+                <div className={styles.desc} >提现，修改密码，及安全设置时用以输入谷歌验证码</div >
+                <div className={styles.button} >修改</div >
+              </li >
+            </ul >
+          </div >
         </div >
       </div >
     )
