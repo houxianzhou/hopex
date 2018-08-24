15b50b7e23984b4d63381f007a9f987fe64a913d
weixiaoyi <xiaoyi.wei@bcsystech.com>
2018-08-24 15:57:50 +0800

    盘口

--- src/models/global/modal.js
+++ src/models/global/modal.js
@@ -4,8 +4,8 @@ import modelExtend from '@models/modelExtend'
 export default joinModel(modelExtend, {
   namespace: 'modal',
   state: {
-    name: 'dealDetail',
-    state: true,
+    name: '',
+    state: false,
   },
 
   effects: {
