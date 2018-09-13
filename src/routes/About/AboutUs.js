0affdfb8ef3ecb936936a9515d70470d53690e6f
weixiaoyi <xiaoyi.wei@bcsystech.com>
2018-09-13 13:38:04 +0800

    关于我们

--- src/routes/About/AboutUs.js
+++ src/routes/About/AboutUs.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'dva'
 import { Mixin } from '@components'
 
-import * as styles from '@routes/Help/index.less'
+import * as styles from '@routes/About/index.less'
 
 @connect(({  Loading, dispatch, }) => ({
   dispatch,
