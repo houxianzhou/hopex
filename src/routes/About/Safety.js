c0e4ac52d71d707f58c29f278b75f88f2ec1c202
weldon <wxh199311@outlook.com>
2018-09-17 14:57:39 +0800

    ui还原

--- src/routes/About/Safety.js
+++ src/routes/About/Safety.js
@@ -29,7 +29,7 @@ export default class View extends Component {
           <div className={styles.aboutTitle} >安全性</div >
           <p className={styles.black} >Hoepx致力于平台及其用户的安全性。 我们信奉严谨、保守、安全的措施, 不会因方便而危及安全。</p >
           <p className={styles.title} >钱包安全性</p >
-          <p className={styles.content} >Hopex史无前例地使用多签名地址来存款和提款。 所有 BitMEX 地址都是多重签名，所有存储都保持离线状态。<br />
+          <p className={styles.content} >Hopex史无前例地使用多签名地址来存款和提款。 所有 Hopex 地址都是多重签名，所有存储都保持离线状态。<br />
             即使系统被黑客完全入侵，包括网页服务器，交易引擎和数据库，黑客亦没有足够的密钥来窃取资金。<br />
             再者，在发送之前，Hopex的每次提款都由至少两名Hopex员工手工审计。 任何云服务器上都没有私钥，所有资金都使用深度冷存储。<br />
             由Hoepx系统发送的所有存款地址，都被第三方服务器验证，以确保他们包含由创始人控制的钥匙。 如果公钥不匹配，系统将立即停机，停止所有交易。</p >
